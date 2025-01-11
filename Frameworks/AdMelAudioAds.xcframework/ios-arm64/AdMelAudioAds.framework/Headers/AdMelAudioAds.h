760b5af18fa7a08f7957b5a1d5ba9335e231553f
Arihide <arihide.takahashi@gmail.com>
2025-01-12 01:09:36 +0900

    Initial Commit

---
+++ Frameworks/AdMelAudioAds.xcframework/ios-arm64/AdMelAudioAds.framework/Headers/AdMelAudioAds.h
@@ -0,0 +1,19 @@
+//
+//  AdMelAudioAds.h
+//  AdMelAudioAds
+//
+//  Created by 高橋現秀 on 2025/01/09.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdMelAudioAds.
+FOUNDATION_EXPORT double AdMelAudioAdsVersionNumber;
+
+//! Project version string for AdMelAudioAds.
+FOUNDATION_EXPORT const unsigned char AdMelAudioAdsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdMelAudioAds/PublicHeader.h>
+
+//#import <AdMelAudioAds/AdMelAudioAds-Swift.h>
+
